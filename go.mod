a9e6d1df40915f5ca0725a6fb37603452996927d
abgr <tbpmtbpm@gmail.com>
2023-06-15 10:55:19 +0330

    v2

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module github.com/itsabgr/s3proxy
+module github.com/itsabgr/s3proxy/v2
 
 go 1.20
 
 require (
 	github.com/aws/aws-sdk-go v1.44.277
 	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/crypto v0.10.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -12,4 +13,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 )
--- s3client.go
+++ s3client.go
@@ -2,48 +2,59 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"io"
-	"os"
+	"time"
 )
 
 type S3Client struct {
-	downloader *s3manager.Downloader
-	bucket     string
-	tmp        string
+	client s3iface.S3API
+	bucket string
 }
 
-func Connect(config *aws.Config, bucket, tmp string) (*S3Client, error) {
-	ses := must(session.NewSession(config))
-	_, err := s3.New(must(session.NewSession(config))).HeadBucket(&s3.HeadBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		return nil, err
+func Connect(config *aws.Config, bucket string, test time.Duration) (*S3Client, error) {
+	cli := s3.New(must(session.NewSession(config)))
+	if test > 0 {
+		ctx, cancel := context.WithTimeout(context.Background(), test)
+		defer cancel()
+		_, err := cli.HeadBucketWithContext(ctx, &s3.HeadBucketInput{
+			Bucket: aws.String(bucket),
+		})
+		if ctx.Err() != nil {
+			return nil, fmt.Errorf("test: could not connect to s3 %q", *config.Endpoint)
+		}
+		if err != nil {
+			return nil, err
+		}
 	}
-	return &S3Client{s3manager.NewDownloader(ses), bucket, tmp}, nil
+	return &S3Client{cli, bucket}, nil
 }
 func (s *S3Client) Download(ctx context.Context, key string) ([]byte, error) {
-	file := must(os.CreateTemp(tempDir, "*.s3proxy"))
-	defer func() {
-		name := file.Name()
-		_ = file.Close()
-		_ = os.Remove(name)
-	}()
-	_, err := s.downloader.DownloadWithContext(ctx, file, &s3.GetObjectInput{
+	//TODO fetch once
+	switch key {
+	case "", " ", "/", ".", "./", "//":
+		return nil, errors.New("invalid key")
+	}
+	response, err := s.client.GetObjectWithContext(ctx, &s3.GetObjectInput{
 		Bucket: &s.bucket,
 		Key:    &key,
 	})
-	if err == nil {
-		return io.ReadAll(file)
+	if err != nil {
+		if err, ok := err.(awserr.Error); ok && err.Code() == s3.ErrCodeNoSuchKey {
+			return nil, nil
+		}
+		return nil, err
 	}
-	if err, ok := err.(awserr.Error); ok && err.Code() == s3.ErrCodeNoSuchKey {
-		return nil, nil
+	defer func() { _ = response.Body.Close() }()
+	content, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.New("could not download: " + err.Error())
 	}
-	return nil, err
-
+	return content, nil
 }
